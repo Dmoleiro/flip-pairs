374dad4e80b5a3275b6cdccc3e3df5cc57c836e9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-07 15:02:41 +0000

    Deploy Dmoleiro/flip-pairs to github.com/Dmoleiro/flip-pairs.git:gh-pages

---
+++ precache-manifest.cb7a7e29e21d6b57d89ff393aa2a96e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8180ffebf976adfcd62d5cf15f9f72d4",
+    "url": "/flip-pairs/index.html"
+  },
+  {
+    "revision": "5d4f541b29dde342aeb3",
+    "url": "/flip-pairs/static/css/main.a2c8aee1.chunk.css"
+  },
+  {
+    "revision": "46e5a35413cf04cac8cc",
+    "url": "/flip-pairs/static/js/2.560ce466.chunk.js"
+  },
+  {
+    "revision": "5d4f541b29dde342aeb3",
+    "url": "/flip-pairs/static/js/main.d2e82745.chunk.js"
+  },
+  {
+    "revision": "cd056ba59c5583eb2b4e",
+    "url": "/flip-pairs/static/js/runtime-main.a1365b90.js"
+  }
+]);
